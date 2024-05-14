65991aa7ca7ef14641bf9775e62ced44e3479201
Philip Hutahaean <109837330+Philiphutahaean@users.noreply.github.com>
2024-05-14 23:38:58 +0700

    a endurance test from cretivox

---
+++ src/Footer/Footer.js
@@ -0,0 +1,125 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-container">
+          <div className="message-for-users">
+            <span>
+              Please contact me in any way <br /> you like
+            </span>
+          </div>
+          <div className="social-links">
+            <div className="social-links-h">Social</div>
+            <div className="social-links-items">
+              <a href="https://www.facebook.com/philip.hutahaean/">Facebook</a>
+              <a href="https://www.instagram.com/philipjh_/">Instagram</a>
+              <a href="https://github.com/Philiphutahaean">Github</a>
+              <a href="https://www.linkedin.com/in/philipjonathanhutahaean/">
+                LinkedIn
+              </a>
+            </div>
+          </div>
+          <div className="social-links">
+            <div className="social-links-h">Contact</div>
+            <div className="social-links-items">
+              <a href="mailto:philiphtahaean1@gmail.com">Mail</a>
+              <a href="https://wa.me/+6285158371107">Whatsapp</a>
+              <a href="https://wa.me/+6285158371107">+6285158371107</a>
+            </div>
+          </div>
+          <div className="footer-menu social-links">
+            <div className="footer-menu-h social-links-h">Other Menu</div>
+            <div className="footer-menu-links social-links-items">
+              <p className="footer-menu-links-items">Home.</p>
+              <p className="footer-menu-links-items">About.</p>
+              <p className="footer-menu-links-items">Projects.</p>
+              <p className="footer-menu-links-items">Contact.</p>
+            </div>
+          </div>
+          <div className="footer-btn">
+            <a
+              href="https://www.linkedin.com/in/philipjonathanhutahaean/"
+              className="footer-btn-a"
+            >
+              Get Started!
+            </a>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <div className="left-footer-bottom">
+            <p>Bandung,Indonesia</p>
+          </div>
+          <div className="right-footer-bottom">
+            <p>2024. Philip Jonathan Hutahaean Made By 100% 💖</p>
+          </div>
+        </div>
+      </div>
+      <div className="footer-ticker">
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+        <div className="footer-ticker-item">
+          &nbsp; - Philip Jonathan Hutahaean{" "}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
